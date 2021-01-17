f866a0ab966d337f49dba847c659e458830ab8d7
Osemin <72602339+osemin@users.noreply.github.com>
2021-01-17 23:24:38 +0300

    Update deneme.php

--- deneme.php
+++ deneme.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body >
-<a href="javascript:alert('Böte web sayfası açılıyor');">
+<a href="javascript:alert('Böyle web sayfası açılıyor');">
 Böte
 </a>
 <!--linkin üstüne geldiğinde durum çubuğunda böte web sayfası açılıyor yazar.-->
